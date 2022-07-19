7368373a3d5a258bde60ae6123daa26419f3e04a
Łukasz Osial <lukasz.osial@gmail.com>
2022-07-19 12:52:25 +0200

    updating routes, adding logo and nav to header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.scss';
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./App.scss";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import Detail from "./pages/Detail";
 
 function App() {
-  return (
-    <div>
-     App
-    </div>
-  );
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route path='/:category/search/:keyword' element={<Catalog />} />
+				<Route path='/:category/:id' element={<Detail />} />
+				<Route path='/:category' element={<Catalog />} />
+				<Route path='/' exact element={<Home />} />
+			</Routes>
+      <Footer/>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,22 @@
 import React from 'react';
+import './header.scss';
+
+import logo from '../../assets/tmovie.png';
+
+const headerNav = [
+  {
+    display: 'Home',
+    path: '/'
+  },
+  {
+    display: 'Movies',
+    path: '/movie'
+  },
+  {
+    display: 'TV Series',
+    path: '/tv'
+  }
+];
 
 const Header = () => {
   return (
