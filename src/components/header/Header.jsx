8adc638bdbc7aa792940de244a864f64c669c2db
Łukasz Osial <lukasz.osial@gmail.com>
2022-07-20 16:52:18 +0200

    updating header and adding button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -21,46 +21,44 @@ const headerNav = [
 
 const Header = () => {
 	const { pathname } = useLocation();
-  const headerRef = useRef(null);
+	const headerRef = useRef(null);
 
-  const active = headerNav.findIndex(e => e.path === pathname);
+	const active = headerNav.findIndex(e => e.path === pathname);
 
-  useEffect(() => {
-    const shrinkHeader = () => {
-      if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        headerRef.current.classList.add('shrink');
-      } else {
-        headerRef.current.classList.remove('shrink');
-      }
-    }
-    window.addEventListener('scroll'. shrinkHeader);
-    return () => {
-      window.removeEventListener('scroll', shrinkHeader);
-    }
-  }, [])
-  
+	useEffect(() => {
+		const shrinkHeader = () => {
+			if (
+				document.body.scrollTop > 100 ||
+				document.documentElement.scrollTop > 100
+			) {
+				headerRef.current.classList.add("shrink");
+			} else {
+				headerRef.current.classList.remove("shrink");
+			}
+		};
+		window.addEventListener("scroll", shrinkHeader);
+		return () => {
+			window.removeEventListener("scroll", shrinkHeader);
+		};
+	}, []);
 
 	return (
-    <div ref={headerRef} className="header">
-      <div className="header_wrap container">
-        <div className="logo">
-          <img src={logo} alt="" />
-          <Link to="/">Movies App</Link>
-        </div>
-        <ul className="header__nav">
-          {
-            headerNav.map((e, i) => (
-              <li key={i} className={`${i === active ? 'active' : ''}`}>
-                <Link to={e.path}>
-                  {e.display}
-                </Link>
-              </li>
-            ))
-          }
-        </ul>
-      </div>
-    </div>
-  );
+		<div ref={headerRef} className='header'>
+			<div className='header__wrap container'>
+				<div className='logo'>
+					<img src={logo} alt='' />
+					<Link to='/'>Movies App</Link>
+				</div>
+				<ul className='header__nav'>
+					{headerNav.map((e, i) => (
+						<li key={i} className={`${i === active ? "active" : ""}`}>
+							<Link to={e.path}>{e.display}</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
