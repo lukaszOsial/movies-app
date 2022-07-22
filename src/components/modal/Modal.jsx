dd20e10819f06ec3902bf30af40aad87c7de68be
Łukasz Osial <lukasz.osial@gmail.com>
2022-07-22 11:22:00 +0200

    styling modal component

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -22,4 +22,26 @@ Modal.propTypes = {
 	id: PropTypes.string,
 };
 
+export const ModalContent = props => {
+
+    const contentRef = useRef(null);
+    const closeModal = () => {
+        contentRef.current.parentNode.classList.remove('active');
+        if(props.onClose) props.onClose();
+    }
+
+    return (
+        <div ref={contentRef} className="modal__content">
+            {props.children}
+            <div className="modal__content__close" onClick={closeModal}>
+                <i className="bx bx-x"></i>
+            </div>
+        </div>
+    )
+}
+
+ModalContent.propTypes = {
+    onClose: PropTypes.func
+}
+
 export default Modal;
