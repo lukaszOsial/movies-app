f5978f812fdac8dfbaa8de899fa349d3804ff281
Łukasz Osial <lukasz.osial@gmail.com>
2022-07-23 11:55:11 +0200

    add HeroSlideItem component

--- src/components/hero-slide/HeroSlide.jsx
+++ src/components/hero-slide/HeroSlide.jsx
@@ -1,54 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import SwiperCore, { Autoplay } from 'swiper';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
-import tmdbApi, { category , movieType } from '../../api/tmdbApi';
-import apiConfig from '../../api/apiConfig';
-import './hero-slide.scss';
+import React, { useState, useEffect, useHistory } from "react";
+import SwiperCore, { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import tmdbApi, { category, movieType } from "../../api/tmdbApi";
+import apiConfig from "../../api/apiConfig";
+import Button, { OutlineButton } from '../button/Button';
+import "./hero-slide.scss";
 
 const HeroSlide = () => {
+	SwiperCore.use([Autoplay]);
 
-    SwiperCore.use([Autoplay]);
+	const [movieItems, setMovieItems] = useState([]);
 
-    const [movieItems, setMovieItems] = useState([]);
+	useEffect(() => {
+		const getMovies = async () => {
+			const params = { page: 1 };
+			try {
+				const response = await tmdbApi.getMoviesList(movieType.popular, {
+					params,
+				});
+				setMovieItems(response.results.slice(1, 4));
+				console.log(response);
+			} catch {
+				console.log("error");
+			}
+		};
+		getMovies();
+	}, []);
 
-    useEffect(() => {
-      const getMovies = async () => {
-        const params = {page: 1}
-        try {
-            const response = await tmdbApi.getMoviesList(movieType.popular, {params});
-            setMovieItems(response.results.slice(0, 4));
-            console.log(response);
-        } catch {
-            console.log('error');
-        }
-      }
-      getMovies();
-    }, []);
-    
-  return (
-    <div className="hero-slide">
-            <Swiper
-                modules={[Autoplay]}
-                grabCursor={true}
-                spaceBetween={0}
-                slidesPerView={1}
-            >
-                {
-                    movieItems.map((item, i) => (
-                        <SwiperSlide key={i}>
-                            {({ isActive }) => (
-                                <img src={apiConfig.originalImage(item.backdrop_path)} />
-                            )}
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
-           
-        </div>
-    
-  )
-}
+	return (
+		<div className='hero-slide'>
+			<Swiper
+				modules={[Autoplay]}
+				grabCursor={true}
+				spaceBetween={0}
+				slidesPerView={1}
+				//autoplay={{delay: 3500}}
+			>
+				{movieItems.map((item, i) => (
+					<SwiperSlide key={i}>
+						{({ isActive }) => (
+							<HeroSlideItem item={item} className={`${isActive ? 'active' : ''}`} />
+						)}
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+};
+
+const HeroSlideItem = props => {
+	//let history = useHistory();
+
+	const item = props.item;
+
+	const background = apiConfig.originalImage(
+		item.backdrop_path ? item.backdrop_path : item.poster_path
+	);
+
+	return (
+		<div
+			className={`hero-slide__item ${props.className}`}
+			style={{ backgroundImage: `url(${background})` }}
+        >
+			<div className="hero-slide__item__content container">
+                <div className="hero-slide__item__content info">
+                    <h2 className="title">
+                        {item.title}
+                    </h2>
+                    <div className="overview">
+                        {item.overview}
+                    </div>
+                    <div className="btns">
+                        <Button>
+
+                        </Button>
+                    </div>
+                </div>
+                <div className="hero-slide__item__content poster"></div>
+            </div>
+		</div>
+	);
+};
 
 export default HeroSlide;
