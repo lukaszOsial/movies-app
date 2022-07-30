2b1cb859e59f098be825c13e3be0cffdfd387203
Łukasz Osial <lukasz.osial@gmail.com>
2022-07-30 07:12:50 +0200

    commit

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import PageHeader from '../components/page-header/PageHeader';
 import { category as cate } from '../api/tmdbApi';
+import MovieGrid from '../components/movie-grid/MovieGrid';
 
 const Catalog = () => {
 
@@ -12,6 +13,11 @@ const Catalog = () => {
         <PageHeader>
           {category === cate.movie ? 'Movies' : 'TV Series'}
         </PageHeader>
+        <div className="container">
+          <div className="section mb-3">
+            <MovieGrid category={category} />
+          </div>
+        </div>
     </>
   )
 }
